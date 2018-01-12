6a6c63a73ac26b7925a786e1a0665db3aeb08e5c
lihaitao <1775814590@qq.com>
2018-01-12 23:19:48 +0800

    完成：dev-server使用

---
+++ webpack4/webpack.config.js
@@ -0,0 +1,22 @@
+const path = require("path");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+module.exports = {
+    entry: "./main.js",
+    output: {
+        filename: "bundle.js",
+        path: path.resolve(__dirname, "./dist")
+    },
+    module: {
+        rules: [{
+            test: /\.css$/,
+            loaders:ExtractTextPlugin.extract({
+                use:['css-loader']
+            })
+        }]
+    },
+    plugins:[
+        new ExtractTextPlugin({
+            filename:`[name]_[contenthash:8].css`,
+        }),
+    ]
+}
