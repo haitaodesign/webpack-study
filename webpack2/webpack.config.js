dc5c91551014c1f00969fd58fdd54fb7bdef5503
lihaitao <1775814590@qq.com>
2018-01-12 22:12:14 +0800

    完成：使用loader

---
+++ webpack2/webpack.config.js
@@ -0,0 +1,18 @@
+const path = require("path");
+module.exports = {
+    entry: "./main.js",
+    output: {
+        filename: "bundle.js",
+        path: path.resolve(__dirname, "./dist")
+    },
+    module: {
+        rules: [{
+            test: /\.css$/,
+            use: [{
+                loader: 'style-loader'
+            }, {
+                loader: 'css-loader',
+            }]
+        }]
+    }
+}
