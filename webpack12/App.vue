7070ae15e678d3a9aa37c9933e4f64499e549feb
lihaitao <1775814590@qq.com>
2018-01-18 23:17:14 +0800

    新增：构建vue应用

---
+++ webpack12/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+      <h1>{{msg}}</h1>
+  </div>
+</template>
+<script>
+export default {
+  data(){
+      return{
+          msg:"hello,vue"
+      }
+  }
+}
+</script>
+<style scoped>
+h1{
+    color: red;
+}
+</style>
+
